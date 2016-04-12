af0cc5d3cc25e4d2c540df54a6765cc9cec56697
s14003 <s14003@std.it-college.ac.jp>
2016-04-12 14:43:55 +0900

    1-5-A end

---
+++ ITP1_5/ITP1_5_A.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        while(true) {
+            int H = scan.nextInt();
+            int W = scan.nextInt();
+            if (H == 0 || W == 0) {
+                break;
+            }
+            for (int i = 0;i < H; i++) {
+                for (int j = 0;j < W; j++) {
+                    sb.append("#");
+                }
+                sb.append("\n");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
