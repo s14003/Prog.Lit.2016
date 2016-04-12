00d9193ec86accb9ba26bdfe0f56c9e2ce8267c2
s14003 <s14003@std.it-college.ac.jp>
2016-04-12 14:08:17 +0900

    1_4_D end

--- ITP1_4/ITP1_4_C.java
+++ ITP1_4/ITP1_4_C.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main{
+class ITP1_4_C {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
