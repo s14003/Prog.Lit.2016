0ea883b11c146516cc51398eae4a87787c09d14b
s14003 <s14003@std.it-college.ac.jp>
2016-04-12 12:15:56 +0900

    いらないもの削除

--- ITP1_4/ITP1_4_A.java
+++ ITP1_4/ITP1_4_A.java
@@ -1,13 +1,10 @@
 import java.util.Scanner;
-import java.math.BigDecimal;
 class Main {
  public static void main(String args[]) {
     Scanner scan = new Scanner(System.in);
 
     int a = scan.nextInt();
     int b = scan.nextInt();
-    BigDecimal big = new BigDecimal(a);
-    BigDecimal decimal = new BigDecimal(b);
 
     int division = a / b;
     int remainder = a % b;
