4a39a1fe430383daa552e365fa88bfb0f86aeb21
s14003 <s14003@std.it-college.ac.jp>
2016-04-13 15:32:16 +0900

    1-7-A end

---
+++ ITP1_7/ITP1_7_A.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        while(true) {
+            int m = scan.nextInt();
+            int f = scan.nextInt();
+            int r = scan.nextInt();
+            if (m == -1 && f == -1 && r == -1) {
+                break;
+            }
+            if ((m + f) >= 80) {
+                sb.append("A").append("\n");
+            } else if ((m + f) >= 65 && (m + f) <= 79) {
+                sb.append("B").append("\n");
+            } else if (m + f >= 50 && (m + f) <= 64) {
+                sb.append("C").append("\n");
+            } else if((m + f) >= 30 && (m + f) <= 49) {
+                sb.append("D").append("\n");
+            } else if ((m + f) >= 30 && (m + f) <= 49 && r >= 50) {
+                sb.append("C").append("\n");
+            } else  {
+                sb.append("F").append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+}
