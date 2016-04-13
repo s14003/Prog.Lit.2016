41fba3bfa9b179174e32484bf44e1e79b8a3988c
s14003 <s14003@std.it-college.ac.jp>
2016-04-13 15:04:56 +0900

    1-6-D end

--- ITP1_6/ITP1_6_C.java
+++ ITP1_6/ITP1_6_C.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-class Main {
+class ITP1_6_C {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
