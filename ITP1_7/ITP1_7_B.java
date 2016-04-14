dd7031005097deeabefe946da9d9f5b1207e96d5
s14003 <s14003@std.it-college.ac.jp>
2016-04-14 09:11:42 +0900

    1-7-B end

--- ITP1_7/ITP1_7_B.java
+++ ITP1_7/ITP1_7_B.java
@@ -2,25 +2,22 @@ import java.util.Scanner;
 class Main {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-        int count = 0;
         while (true) {
             int n = scan.nextInt();
             int x = scan.nextInt();
 
-            if (n == 0 && x == 0) {
-                break;
-            }
-            for (int i = 1;i < n;i++) {
-                for (int j = i; j < n;j++) {
-                    if(i == j) {
+            if(n==0 && x==0) break;
+            int count=0;
+            for(int i=1; i<n; ++i){
+                for(int j=i; j<n; ++j){
+                    if(i==j) {
                         continue;
                     }
-                    int num = x - (i + j);
-                    if (num < j) {
+                    int num = x - (i+j);
+                    if(num<1) {
                         break;
                     }
-                    if (num < 1) {
+                    if(num<j) {
                         break;
                     }
                     if(i != num && j != num && num <= n) {
@@ -29,7 +26,6 @@ class Main {
                     num = 0;
                 }
             }
-        }
-        System.out.println(count);
+            System.out.println(count);}
     }
 }
