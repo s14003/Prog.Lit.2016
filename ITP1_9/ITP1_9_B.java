5384220af631b0c2cf5c6ab123cecc31c9caa53e
s14003 <s14003@std.it-college.ac.jp>
2016-04-15 10:59:58 +0900

    1-9-B end

--- ITP1_9/ITP1_9_B.java
+++ ITP1_9/ITP1_9_B.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Main {
+class ITP1_9_B {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
